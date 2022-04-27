f53c0f59fc44f6191d13c01f49508a59be140482
shermaine <shermaine.lim.si.hui@gmail.com>
2022-04-27 14:39:56 +0800

    Commit 1
    Migration

---
+++ src/store/categories/category.action.js
@@ -0,0 +1,25 @@
+import { createAction } from "../../utils/reducer/reducer.utils";
+import CATEGORIES_ACTION_TYPE from "./category.types";
+import { getCategoriesAndDocuments } from "../../utils/firebase/firebase.utils";
+
+export const fetchCategoriesStart = () =>
+  createAction(CATEGORIES_ACTION_TYPE.FETCH_CATEGORIES_START);
+
+export const fetchCategoriesSuccess = (categoriesArray) =>
+  createAction(
+    CATEGORIES_ACTION_TYPE.FETCH_CATEGORIES_SUCCESS,
+    categoriesArray
+  );
+
+export const fetchCategoriesFailed = (error) =>
+  createAction(CATEGORIES_ACTION_TYPE.FETCH_CATEGORIES_FAILED, error);
+
+export const fetchCategoriesAsync = () => async (dispatch) => {
+  dispatch(fetchCategoriesStart());
+  try {
+    const categoryArray = await getCategoriesAndDocuments("categories");
+    dispatch(fetchCategoriesSuccess(categoryArray));
+  } catch (error) {
+    dispatch(fetchCategoriesFailed(error));
+  }
+};
---
+++ src/store/store.js
@@ -0,0 +1,35 @@
+import { compose, createStore, applyMiddleware } from "redux";
+import logger from "redux-logger";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import { rootReducer } from "./root-reducer";
+import thunk from "redux-thunk";
+
+const middleWares = [
+  process.env.NODE_ENV === "development" && logger,
+  thunk,
+].filter(Boolean);
+
+const composeEnhancer =
+  (process.env.NODE_ENV !== "production" &&
+    window &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
+
+const persistConfig = {
+  key: "root",
+  storage,
+  white: ["cart"],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
+
+export const store = createStore(
+  persistedReducer,
+  undefined,
+  composedEnhancers
+);
+
+export const persistor = persistStore(store);
