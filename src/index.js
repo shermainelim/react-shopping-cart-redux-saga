8678755aed48bc86ec0cd29129292f26b2bb4096
shermaine <shermaine.lim.si.hui@gmail.com>
2022-04-29 21:13:08 +0800

    stripe Promise

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter } from "react-router-dom";
 import { Provider } from "react-redux";
 import { PersistGate } from "redux-persist/integration/react";
 import { Elements } from "@stripe/react-stripe-js";
+import { stripePromise } from "./utils/stripe/stripe.utils";
 
 import App from "./App";
 import { store, persistor } from "./store/store";
@@ -17,7 +18,7 @@ render(
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <BrowserRouter>
-          <Elements>
+          <Elements stripe={stripePromise}>
             <App />
           </Elements>
         </BrowserRouter>
